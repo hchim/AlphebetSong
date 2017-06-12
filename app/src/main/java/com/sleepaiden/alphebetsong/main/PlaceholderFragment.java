8e27d6732924860f1752dea84e86407e9cda4402
Hui Chen <hchen229@gmail.com>
2017-06-12 06:35:21 -0700

    Add voice recording.

--- app/src/main/java/com/sleepaiden/alphebetsong/main/PlaceholderFragment.java
+++ app/src/main/java/com/sleepaiden/alphebetsong/main/PlaceholderFragment.java
@@ -1,16 +1,29 @@
 package com.sleepaiden.alphebetsong.main;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.sleepaiden.alphebetsong.R;
 import com.sleepaiden.alphebetsong.models.AlphebetPage;
+import com.sleepaiden.alphebetsong.settings.PreferenceConstants;
+import com.sleepaiden.androidcommonutils.PreferenceUtils;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,11 +32,20 @@ import butterknife.ButterKnife;
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
+    private static final String TAG = "PlaceholderFragment";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     private AlphebetPage alphebetPage;
+    private PreferenceUtils preferenceUtils;
+    private String learningMode;
 
     @BindView(R.id.imageView) ImageView imageView;
     @BindView(R.id.wordTextView) TextView textView;
+    @BindView(R.id.voiceButton) ImageButton voiceBtn;
+    @BindView(R.id.alphebetToolbar) LinearLayout alphebetToolbar;
+
+    private MediaRecorder mRecorder;
+    private String voiceFilePath;
 
     /**
      * The fragment argument representing the section number for this
@@ -48,6 +70,13 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        voiceFilePath = getActivity().getExternalCacheDir().getAbsolutePath()
+                + String.format("/astemp-%d.3gp", System.currentTimeMillis());
+        preferenceUtils = new PreferenceUtils(getContext());
+        learningMode = preferenceUtils.getString(
+                PreferenceConstants.PREF_KEY_LEARNING_MODE,
+                PreferenceConstants.LEARNING_MODE_MANUAL);
+
         View rootView = inflater.inflate(R.layout.fragment_page, container, false);
         ButterKnife.bind(this, rootView);
         alphebetPage = getArguments().getParcelable(ARG_ALPHEBET_PAGE);
@@ -57,6 +86,80 @@ public class PlaceholderFragment extends Fragment {
                 .into(imageView);
         textView.setText(alphebetPage.getWord());
 
+        voiceBtn.setOnTouchListener(voiceBtnTouchListener);
+
+        if (learningMode.equals(PreferenceConstants.LEARNING_MODE_AUTOMATIC)) {
+            alphebetToolbar.setVisibility(View.INVISIBLE);
+        }
+
         return rootView;
+    }
+
+    private View.OnTouchListener voiceBtnTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getAction();
+            if (action == MotionEvent.ACTION_DOWN) {
+                int permission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.RECORD_AUDIO);
+                if (permission == PackageManager.PERMISSION_GRANTED) {
+                    startRecording();
+                } else {
+                    requestRecordAudioPermission();
+                }
+            } else if (action == MotionEvent.ACTION_UP) {
+                stopRecording();
+            }
+
+            return false;
+        }
+    };
+
+    private void requestRecordAudioPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)) {
+            } else {
+                requestPermissions(
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        REQUEST_RECORD_AUDIO_PERMISSION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(voiceFilePath);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "Prepare media recorder failed.", e);
+        }
+
+        mRecorder.start();
+        Log.d(TAG, "Start recording...");
+    }
+
+    private void stopRecording() {
+        if (mRecorder != null) {
+            try {
+                mRecorder.stop();
+                VoiceFragmentDialog.createInstance(voiceFilePath).show(getFragmentManager(), null);
+            } catch (RuntimeException e) {
+                Log.e(TAG, "Failed to stop voice recorder.", e);
+            } finally {
+                mRecorder.release();
+                mRecorder = null;
+            }
+        }
+        Log.d(TAG, "Stop recording...");
     }
 }
---
+++ app/src/main/java/com/sleepaiden/alphebetsong/main/VoiceFragmentDialog.java
@@ -0,0 +1,115 @@
+package com.sleepaiden.alphebetsong.main;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+import com.sleepaiden.alphebetsong.R;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class VoiceFragmentDialog extends DialogFragment {
+    private static final String ARG_VOICE_FILE = "alphebet.voice_file";
+
+    @BindView(R.id.playButton) ImageButton playBtn;
+    @BindView(R.id.progressBar) ProgressBar progressBar;
+
+    private String voiceFile;
+    private MediaPlayer mPlayer;
+    private boolean isPlaying = false;
+
+    public static VoiceFragmentDialog createInstance(String voiceFile) {
+        VoiceFragmentDialog dialog = new VoiceFragmentDialog();
+        Bundle bundle = new Bundle();
+        bundle.putString(ARG_VOICE_FILE, voiceFile);
+        dialog.setArguments(bundle);
+        return dialog;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        voiceFile = getArguments().getString(ARG_VOICE_FILE);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_show_voice, null, false);
+        ButterKnife.bind(this, view);
+
+        builder.setView(view)
+                .setPositiveButton(R.string.save_button, null)
+                .setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        VoiceFragmentDialog.this.getDialog().cancel();
+                    }
+                });
+        final AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                final Button button = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+        });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlaying();
+            }
+        });
+
+        return dialog;
+    }
+
+    private void startPlaying() {
+        if (isPlaying) {
+            stopPlaying();
+        } else {
+            mPlayer = new MediaPlayer();
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlaying();
+                }
+            });
+
+            try {
+                mPlayer.setDataSource(voiceFile);
+                mPlayer.prepare();
+                mPlayer.start();
+                playBtn.setBackgroundResource(R.drawable.ic_pause_48dp);
+                isPlaying = true;
+            } catch (IOException e) {
+                Log.e("VoiceFragmentDialog", "prepare play voice failed.");
+            }
+        }
+    }
+
+    private void stopPlaying() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+        playBtn.setBackgroundResource(R.drawable.ic_play_48dp);
+        isPlaying = false;
+    }
+}
